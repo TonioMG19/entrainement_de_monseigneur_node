dab04ce5aa59fbb71d2fd1be8ff418c6d5862bbc
TonioMG19 <toniokuhn2@gmail.com>
2020-04-14 14:46:26 +0400

    exo 2

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-/* Exercice 1 */
-const age = 17;
+/*Exercice 1*/
+/*const age = 18;
 if(age>=18){
   console.log('Majeur');
 } else {
   console.log('Mineur');
+}*/
+
+/*Exercice 2*/
+const color = 'bleu';
+switch (color){
+  case 'bleu':
+    console.log('La couleur est bleu');
+    break;
+  case 'rouge':
+    console.log('La couleur est rouge');
+    break;
+  case 'vert':
+    console.log('La couleur est vert');
+    break;
+  case 'jaune':
+    console.log('La couleur est jaune');
+    break;
+  default:
+    console.log(`La couleur était ${color}`)
 }
